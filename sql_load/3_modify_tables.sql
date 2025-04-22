e72799b2f1b1b6021f6df3bec78e7ffcf7020b1d
Telemack-90 <berhane90@gmail.com>
2025-04-22 17:24:35 +0200

    push example

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,8 +1,16 @@
-SELECT COUNT(job_id) AS total,
-    EXTRACT(
-        MONTH
-        from job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York'
-    ) AS mois
-FROM job_postings_fact
-GROUP BY mois
-ORDER BY mois DESC;
+WITH remote_job_skills AS (
+    SELECT skill_id,
+        COUNT(skills_job_dim.job_id) AS skill_count
+    FROM skills_job_dim
+        INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_job_dim.job_id
+    WHERE job_postings.job_work_from_home = TRUE
+        AND job_postings.job_title_short = 'Data Analyst'
+    GROUP BY skill_id
+)
+SELECT skills.skill_id,
+    skills AS skill_name,
+    skill_count
+FROM remote_job_skills
+    INNER JOIN skills_dim AS skills ON skills.skill_id = remote_job_skills.skill_id
+ORDER BY skill_count DESC
+LIMIT 5;
